00da970230cfdbd61c0c352c77046998531d8ad1
Kevin <calvinsongele@gmail.com>
2026-09-18 13:29:04 +0300

    i

--- index.php
+++ index.php
@@ -6,13 +6,12 @@ require __DIR__ . "/vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-
+ 
 require 'config.php';
 require 'util/Auth.php';
 $is_under_maintenace = 0;
 $approved_ips = ['41.90.210.158', '41.90.210.124', '41.90.211.21'];
-
-//https://demo.templatemonster.com/demo/511124.html
+ 
   
     if ( strpos($_SERVER['REQUEST_URI'], '.php') !== false )  {
         die("
